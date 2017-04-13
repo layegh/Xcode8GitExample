fbdf6608c42f213c7475372d3aa5501baed8fd45
Kamran Layegh <kamran27@einstein.local>
2017-04-13 17:38:28 +0100

    Initial Commit

---
+++ Xcode8GithubExmple/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GithubExmple
+//
+//  Created by Kamran Layegh on 13/04/2017.
+//  Copyright © 2017 Ethical Applications. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
